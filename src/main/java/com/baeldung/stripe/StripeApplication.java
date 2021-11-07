81401d9b6d5eaad0488314f4d2d142ec45668eeb
Sualeh Fatehi <sualeh@hotmail.com>
2021-11-07 00:09:31 -0400

    Tweaks

--- src/main/java/com/baeldung/stripe/StripeApplication.java
+++ src/main/java/com/baeldung/stripe/StripeApplication.java
@@ -2,11 +2,24 @@ package com.baeldung.stripe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class StripeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(StripeApplication.class, args);
-    }
+  public static void main(final String[] args) {
+    SpringApplication.run(StripeApplication.class, args);
+  }
+
+  @Bean
+  public CommonsRequestLoggingFilter logFilter() {
+    final CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+    filter.setIncludeQueryString(true);
+    filter.setIncludePayload(true);
+    filter.setMaxPayloadLength(10000);
+    filter.setIncludeHeaders(false);
+    filter.setAfterMessagePrefix("REQUEST DATA : ");
+    return filter;
+  }
 }
